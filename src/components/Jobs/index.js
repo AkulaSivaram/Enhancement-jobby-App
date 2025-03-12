3cb1ea0e5f70bb2a4caa04e09928415a29927e6d
AkulaSivaram <akulasivaram84@gamail.com>
2025-03-12 06:10:18 +0000

    solution

---
+++ src/components/Jobs/index.js
@@ -0,0 +1,18 @@
+import './index.css'
+
+import Header from '../Header'
+
+import AllJobSection from '../AllJobSection'
+
+const Jobs = () => (
+  <>
+    <Header />
+    <div className="jobs-container">
+      <div className="jobs-sections">
+        <AllJobSection />
+      </div>
+    </div>
+  </>
+)
+
+export default Jobs
